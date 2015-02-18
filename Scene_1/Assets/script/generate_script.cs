ef69154d5ac601c4cfec0bd910df737eece2f582
Zimpo <paul.ziwiakowsky@epita.fr>
2015-02-18 15:15:09 +0100

    bon ba y a pas mal de truc donc faut regarder c que du code
    1. generateur de map
    2. creation d'un personnage
    3. deplacement d'un personnage
    4. changement de couleur pour les cases deja colorer

---
+++ Scene_1/Assets/script/generate_script.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class generate_script : MonoBehaviour {
+	public Transform[] _stock;//stock les elements generer
+	public TextAsset _textMap;//fichier texte
+	public GameObject[] _element;//element a placer dans unity
+
+
+	private GameObject ajouter;
+
+	private static GameObject[] _sol = new GameObject[100];
+	private string text;
+	private float originalx;
+	private float originaly;
+	private float originalz;
+	private float x = 0;
+	private float y = 0;
+	private float z = 0;
+	/// Use this for initialization
+	void Start () {
+				originalx = x;
+				originaly = y;
+				originalz = z;
+				text = _textMap.text;
+	
+	
+				for (int i = 0; i < text.Length; i++) {
+			int j = 0;
+						if (text.Substring (i, 1) == "s") {
+								ajouter = (GameObject)Instantiate (_element [0], new Vector3 (x, y, z), Quaternion.identity);
+								//Debug.Log(tabPos[i][0]);
+								ajouter.transform.parent = _stock [0];
+								x = x + 1;
+				_sol[j] = ajouter;//on met le gameobject sol dans un tableau pour pouvoir changer sa couleur et tout ...
+				j = j+1;
+				//Debug.Log(_sol[i]);
+						} else if (text.Substring (i, 1) == "m") {
+								ajouter = (GameObject)Instantiate (_element [1], new Vector3 (x, y, z), Quaternion.identity);
+								ajouter.transform.parent = _stock [1];
+								x = x + 1;
+						} else if (text.Substring (i, 1) == "z") {
+								x = originalx;
+								z = z + 1;	
+						}
+				}
+		}
+	public static void changeColor(GameObject objet){
+
+		if (objet.renderer.material.color == Color.cyan) {
+			objet.renderer.material.color = Color.white;
+				}
+		else {
+			objet.renderer.material.color = Color.cyan;
+				}
+
+	
+	}
+	// Update is called once per frame
+	void Update () {
+				//if (Input.GetMouseButtonDown(0)) {
+				//	x=tabPos[13][0];
+				//	y=1;
+				//	z=tabPos[13][2];
+
+				//	GameObject ajouter = (GameObject)Instantiate(_element[2],new Vector3(x,y,z) , Quaternion.identity);
+				
+		}
+		 
+	
+	}
+
+
---
+++ Scene_1/Assets/script/perso_script.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class perso_script : MonoBehaviour {
+	public GameObject _perso; //dans prefabs c personnage
+	public Transform _addPerso;//correspond a posperso
+
+	private float xOrigin;
+	private float zOrigin;
+	private float x;
+	private float z;
+	//position du personnage
+	private GameObject ajouter;
+	//permet de mettre le perso dans un objet que l'on peut deplacer
+	private int create = 0;
+	// Use this for initialization
+
+	void Start () {
+
+		createPerso ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deplacePerso ();
+	
+	}
+	void createPerso(){
+
+		_addPerso.transform.position = new Vector3(1,1.5F,1);
+		ajouter = (GameObject)Instantiate (_perso,new Vector3(1,1.5F,1), Quaternion.identity);
+		ajouter.transform.parent = _addPerso;
+		xOrigin = 1.0F;
+		zOrigin = 1.0F;
+
+		}
+	void deplacePerso()
+	{
+				if (Input.GetMouseButtonDown (0) && create == 0) {
+						Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+						//ray = lance un fil, camer...=origine , inpu...=point ou on le lance
+						RaycastHit hit;
+						//cree pour donner un game object dedant
+						if (Physics.Raycast (ray, out hit) && hit.point.x < 6.5 && hit.point.x > 0.5 && hit.point.z < 6.5 && hit.point.z > 0.5) {
+								//physics...=verifie si l'on touche un objet // hit point correspond a la taille de la map
+								
+								/*_addPerso.transform.position = hit.point;
+								ajouter = (GameObject)Instantiate (_perso, hit.point, Quaternion.identity);
+								ajouter.transform.parent = _addPerso;
+								Debug.Log (hit.point);*/
+								
+								//permet de se deplacer a la position precise toucher par le ray
+
+				var yolo = Physics.OverlapSphere(hit.point, 0);//chope le gameobject toucher par le ray
+
+				generate_script.changeColor(yolo[0].gameObject);
+				x = yolo[0].transform.position.x;
+				z = yolo[0].transform.position.z;
+
+				_addPerso.transform.position = new Vector3(x, 1, z);
+				//_addPerso.rigidbody.AddForce ( new Vector3(x, 1, z)*Time.deltaTime);
+				//ajouter = (GameObject)Instantiate (_perso, new Vector3(x, 1, z), Quaternion.identity);//cree un nouvel objet
+				//ajouter.transform.parent = _addPerso;
+				//Debug.Log(yolo[0].transform.position);
+				// a mettre dans le main au dessus on suppose que c le sol qui a ete toucher
+
+		
+						}
+				}
+		}
+	}
